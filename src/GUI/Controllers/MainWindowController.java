326ab4c5d238663693443063205bb9c97b9eb695
fjt1509 <fjt1509@hotmail.com>
2018-01-09 10:49:08 +0100

    TableView

--- src/GUI/Controllers/MainWindowController.java
+++ src/GUI/Controllers/MainWindowController.java
@@ -5,13 +5,20 @@
  */
 package GUI.Controllers;
 
+import BE.Category;
+import BE.Movie;
+import GUI.Model.Model;
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.String;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -22,9 +29,20 @@ public class MainWindowController implements Initializable
 {
 
     @FXML
-    private TableView<?> catMovieTableView;
+    private TableView catMovieTableView;
     @FXML
-    private TableColumn<?, ?> categoryTableColumn;
+    private TableColumn categoryTableColumn;
+    
+ 
+    private TableView<Category> categoryTableView;
+    private TableColumn<Category, String> categoryTableColumnn;
+    
+    private Model model;
+
+    public MainWindowController() throws SQLException, IOException 
+    {
+        this.model = new Model();
+    }
 
     /**
      * Initializes the controller class.
@@ -32,17 +50,24 @@ public class MainWindowController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+       catMovieTableView = categoryTableView;
+       categoryTableColumn = categoryTableColumnn;
+       
+       catMovieTableView.setItems(model.getAllCategory());
+       categoryTableColumn.setCellValueFactory(new PropertyValueFactory("name"));
     }    
+  
 
     @FXML
     private void addCategoryEvent(ActionEvent event)
     {
+        
     }
 
     @FXML
     private void deleteCategoryEvent(ActionEvent event)
     {
+        
     }
     
 }

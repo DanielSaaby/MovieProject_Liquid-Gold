a631234093878141eaf02f1a61ae04db86a5935a
kENT <pedersenkentjuul@gmail.com>
2018-01-09 11:35:14 +0100

    ActionEvent & TxtField names

--- src/GUI/Controllers/AddMovieControllerController.java
+++ src/GUI/Controllers/AddMovieControllerController.java
@@ -7,7 +7,10 @@ package GUI.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -17,6 +20,15 @@ import javafx.fxml.Initializable;
 public class AddMovieControllerController implements Initializable
 {
 
+    @FXML
+    private TextField movieTitletxtField;
+    @FXML
+    private TextField ratingTxtField;
+    @FXML
+    private TextField genreTxtField;
+    @FXML
+    private TextField filePathTxt;
+
     /**
      * Initializes the controller class.
      */
@@ -25,5 +37,20 @@ public class AddMovieControllerController implements Initializable
     {
         // TODO
     }    
+
+    @FXML
+    private void saveMovieBtn(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void cancelAddMovieBtn(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void chooseFileBtn(ActionEvent event)
+    {
+    }
     
 }
